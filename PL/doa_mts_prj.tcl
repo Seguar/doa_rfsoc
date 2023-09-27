c43132b2745ddc8a64848da9a8248f83e98538a6
Seguar <69475749+Seguar@users.noreply.github.com>
2023-09-27 17:36:54 +0300

    Fixed missing xdc

--- PL/doa_mts_prj.tcl
+++ PL/doa_mts_prj.tcl
@@ -58,6 +58,7 @@ if { $list_projs eq "" } {
 # CHANGE DESIGN NAME HERE
 variable design_name
 set design_name doa_mts
+set origin_dir "."
 
 # Set IP repository paths
 set obj [get_filesets sources_1]
@@ -68,6 +69,20 @@ if { $obj != {} } {
    update_ip_catalog -rebuild
 }
 
+# Set 'constrs_1' fileset object
+set obj [get_filesets constrs_1]
+
+# Add/Import constrs file and set constrs file properties
+set file "[file normalize "$origin_dir/xdc/$design_name.xdc"]"
+set file_added [add_files -norecurse -fileset $obj [list $file]]
+set file "$origin_dir/xdc/*.xdc"
+set file [file normalize $file]
+set file_obj [get_files -of_objects [get_filesets constrs_1] [list "*$file"]]
+set_property -name "file_type" -value "XDC" -objects $file_obj
+
+# Set 'constrs_1' fileset properties
+set obj [get_filesets constrs_1]
+set_property target_constrs_file $origin_dir/$design_name.xdc [current_fileset -constrset]
 # Set 'sources_1' fileset object
 set obj [get_filesets sources_1]
 set files [glob "${origin_dir}/src/*"]
@@ -2433,6 +2448,6 @@ Port;FD4A0000;FD4AFFFF;1|FPD;DPDMA;FD4C0000;FD4CFFFF;1|FPD;DDR_XMPU5_CFG;FD05000
 
 create_root_design ""
 
-make_wrapper -files [get_files ./myproj/project_1.srcs/sources_1/bd/$design_name/$design_name.bd] -top
-add_files -norecurse ./myproj/project_1.gen/sources_1/bd/$design_name/hdl/$design_name\_wrapper.v
+make_wrapper -files [get_files $origin_dir/myproj/project_1.srcs/sources_1/bd/$design_name/$design_name.bd] -top
+add_files -norecurse $origin_dir/myproj/project_1.gen/sources_1/bd/$design_name/hdl/$design_name\_wrapper.v
 update_compile_order -fileset sources_1
